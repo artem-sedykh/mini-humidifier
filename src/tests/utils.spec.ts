394de523a1435d89a3695c16b87a9cb3af092630
Artem Sedykh <anavrin72@gmail.com>
2020-07-16 15:44:03 +0300

    add toggleEntity test

--- src/tests/utils.spec.ts
+++ src/tests/utils.spec.ts
@@ -1,8 +1,11 @@
 import { expect, assert } from 'chai';
-import { isNumeric, parseTapAction } from '../utils/utils';
+import { compileTemplate, isNumeric, parseTapAction } from '../utils/utils';
 import { TapAction } from '../types';
 import { computeDomain } from '../utils/compute-domain';
 import { EmptyPromise } from '../const';
+import { HassEntity } from 'home-assistant-js-websocket';
+import { deepEqual, instance, mock, spy, verify, when } from 'ts-mockito';
+import { toggleEntity } from '../utils/toggle-entity';
 
 describe('utils', () => {
   it('EmptyPromise', () => {
@@ -54,6 +57,146 @@ describe('utils', () => {
   computeDomainTestSource.forEach(function(test) {
     it(`computeDomain: ${test.value}`, () => {
       expect(computeDomain(test.value)).to.equal(test.expected);
+    });
+  });
+
+  const toggleEntityTestSource = [
+    {
+      entityId: 'lock.test',
+      state: 'closed',
+      expected: {
+        domain: 'lock',
+        service: 'unlock',
+        serviceData: { entity_id: 'lock.test' },
+      },
+    },
+    {
+      entityId: 'lock.test',
+      state: 'open',
+      expected: {
+        domain: 'lock',
+        service: 'lock',
+        serviceData: { entity_id: 'lock.test' },
+      },
+    },
+    {
+      entityId: 'switch.test',
+      state: 'on',
+      expected: {
+        domain: 'switch',
+        service: 'turn_off',
+        serviceData: { entity_id: 'switch.test' },
+      },
+    },
+    {
+      entityId: 'switch.test',
+      state: 'off',
+      expected: {
+        domain: 'switch',
+        service: 'turn_on',
+        serviceData: { entity_id: 'switch.test' },
+      },
+    },
+    {
+      entityId: 'cover.test',
+      state: 'on',
+      expected: {
+        domain: 'cover',
+        service: 'close_cover',
+        serviceData: { entity_id: 'cover.test' },
+      },
+    },
+    {
+      entityId: 'cover.test',
+      state: 'off',
+      expected: {
+        domain: 'cover',
+        service: 'open_cover',
+        serviceData: { entity_id: 'cover.test' },
+      },
+    },
+    {
+      entityId: 'group.test',
+      state: 'off',
+      expected: {
+        domain: 'homeassistant',
+        service: 'turn_on',
+        serviceData: { entity_id: 'group.test' },
+      },
+    },
+    {
+      entityId: 'group.test',
+      state: 'on',
+      expected: {
+        domain: 'homeassistant',
+        service: 'turn_off',
+        serviceData: { entity_id: 'group.test' },
+      },
+    },
+  ];
+
+  toggleEntityTestSource.forEach(function(test) {
+    it(`toggleEntity: ${test.entityId}, state: ${test.state}`, () => {
+      const entityMock: HassEntity = mock<HassEntity>();
+      when(entityMock.state).thenReturn(test.state);
+      when(entityMock.entity_id).thenReturn(test.entityId);
+      const entity: HassEntity = instance(entityMock);
+
+      const testObj: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        callService: (domain: string, service: string, serviceData?: { [key: string]: any }) => Promise<void>;
+      } = {
+        callService: EmptyPromise,
+      };
+
+      const spiedTestObj = spy(testObj);
+
+      toggleEntity(entity, testObj.callService);
+
+      verify(
+        spiedTestObj.callService(test.expected.domain, test.expected.service, deepEqual(test.expected.serviceData)),
+      ).once();
+    });
+  });
+
+  const compileTemplateTestSource = [
+    {
+      func: (): string => 'test_value',
+      expected: {
+        result: 'test_value',
+        exception: false,
+      },
+    },
+    {
+      func: "() => 'test_value'",
+      expected: {
+        result: 'test_value',
+        exception: false,
+      },
+    },
+    {
+      func: '() ==> test_value',
+      expected: {
+        result: 'test_value',
+        exception: true,
+      },
+    },
+  ];
+
+  compileTemplateTestSource.forEach(function(test) {
+    it('compileTemplate', () => {
+      try {
+        const func = compileTemplate(test.func);
+        expect(func()).to.equal(test.expected.result);
+      } catch (e) {
+        if (!test.expected.exception) {
+          assert.fail();
+        }
+        return;
+      }
+      if (test.expected.exception) {
+        assert.fail();
+      }
     });
   });
 });
