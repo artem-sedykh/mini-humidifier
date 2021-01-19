6eb5419e4b51952ad7dbd93897d694561f8b07a0
Юрий Аузинь <zuz666@gmail.com>
2021-01-19 18:14:17 +0500

    Add default config for xiaomi_miio:zhimi_humidifier_ca4

--- src/humidifiers.js
+++ src/humidifiers.js
@@ -1,4 +1,5 @@
 import ZHIMI_HUMIDIFIER_CB1 from './configurations/xiaomi_miio/zhimi_humidifier_cb1';
+import ZHIMI_HUMIDIFIER_CA4 from './configurations/xiaomi_miio/zhimi_humidifier_ca4';
 import ZHIMI_AIRPURIFIER_MA2 from './configurations/xiaomi_miio/zhimi_airpurifier_ma2';
 import XIAOMI_MIIO_AIRPURIFIER_DEERMA_HUMIDIFIER_MJJSQ from './configurations/xiaomi_miio_airpurifier/deerma_humidifier_mjjsq';
 import XIAOMI_MIIO_AIRPURIFIER_ZHIMI_HUMIDIFIER_CB1 from './configurations/xiaomi_miio_airpurifier/zhimi_humidifier_cb1';
@@ -8,6 +9,7 @@ const HUMIDIFIERS = {
   default: ZHIMI_HUMIDIFIER_CB1,
   // xiaomi_miio integration (default home assistant)
   'zhimi.humidifier.cb1': ZHIMI_HUMIDIFIER_CB1,
+  'zhimi.humidifier.ca4': ZHIMI_HUMIDIFIER_CA4,
   'zhimi.airpurifier.ma2': ZHIMI_AIRPURIFIER_MA2,
 
   // xiaomi_miio_airpurifier integration https://github.com/syssi/xiaomi_airpurifier
