23afc01f79959a5471d1d92ad3493128153291f8
regevbr <regevbr@gmail.com>
2022-04-10 00:25:45 +0300

    feature: add support for zhimi.airpurifier.mb

--- src/configurations/xiaomi_miio_airpurifier/zhimi_airpurifier_mb3.js
+++ src/configurations/xiaomi_miio_airpurifier/zhimi_airpurifier_mb3.js
@@ -16,7 +16,7 @@ const XIAOMI_MIIO_AIRPURIFIER_ZHIMI_AIRPURIFIER_MB3 = () => ({
     min: 0,
     max: 14,
     step: 1,
-    hide: (state, entity) => (entity.attributes.preset_mode !== 'Favorite'),
+    disabled: (state, entity) => (entity.state !== 'on' || entity.attributes.preset_mode !== 'Favorite'),
     state: { entity: 'input_number.{entity_id}_favorite_level' },
     change_action: (selected, state, entity) => {
       const options = { entity_id: entity.entity_id, value: selected };
@@ -91,8 +91,8 @@ const XIAOMI_MIIO_AIRPURIFIER_ZHIMI_AIRPURIFIER_MB3 = () => ({
           return modes.map(mode => ({ id: mode, name: mode }));
         },
       },
-      active: (state, entity) => (entity.state !== 'off'),
-      disabled: false,
+      active: (state, entity) => (entity.state === 'on'),
+      disabled: (state, entity) => (entity.state !== 'on'),
       state: { attribute: 'preset_mode' },
       change_action: (selected, state, entity) => {
         const options = { entity_id: entity.entity_id, preset_mode: selected };
@@ -109,7 +109,8 @@ const XIAOMI_MIIO_AIRPURIFIER_ZHIMI_AIRPURIFIER_MB3 = () => ({
         2: 'Medium',
         3: 'High',
       },
-      active: (state, entity) => (entity.attributes.preset_mode === 'Fan'),
+      active: (state, entity) => (entity.state === 'on' && entity.attributes.preset_mode === 'Fan'),
+      disabled: (state, entity) => (entity.state !== 'on' || entity.attributes.preset_mode !== 'Fan'),
       state: { attribute: 'fan_level' },
       change_action: (selected, state, entity) => {
         const options = { entity_id: entity.entity_id, level: selected };
